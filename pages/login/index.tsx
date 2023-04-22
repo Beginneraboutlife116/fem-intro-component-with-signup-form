56a9dfdce9a40821b3e2319cc3637c7402c8db4c
Beginneraboutlife116 <pythonforeye@gmail.com>
2023-04-22 14:49:27 +0800

    structure folder

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,18 +1,18 @@
 import Head from "next/head"
-import Link from "next/link"
+import Input from "../../components/input"
+import styles from "./Home.module.scss"
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>FEM-intro component with sign-up form</title>
         <meta name="description" content="intro component with sign-up form" />
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
 
-      <main>
-        {/* How to make the default page is "login" page? */}
-        <Link href="/login">Login</Link>
+      <main className={styles.main}>
+        <Input type="text" placeholder="LastName" id="text" />
       </main>
     </div>
   )
