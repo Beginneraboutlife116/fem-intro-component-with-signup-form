35c0bf77d239587fa5ae2511378734b5940b4f64
Beginneraboutlife116 <pythonforeye@gmail.com>
2023-05-04 14:29:47 +0800

    increase determineInputExceedLimit function

--- components/utilities/index.ts
+++ components/utilities/index.ts
@@ -1,8 +1,10 @@
 const determineEmptyString: (value: string) => boolean = (value) => value === ""
 
+const determineInputExceedsLimit: (limit: number) => (value: string) => boolean = (limit) => (value) => value ? value.length > limit : false
+
 const determineValidateEmail: (value: string) => boolean = (value) => {
   const pattern = new RegExp(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)
   return value !== undefined && !pattern.test(value)
 }
 
-export { determineEmptyString, determineValidateEmail }
+export { determineEmptyString, determineInputExceedsLimit, determineValidateEmail }
