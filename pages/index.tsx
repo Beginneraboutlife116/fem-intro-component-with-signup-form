48216ac39980ccf92c5e0207367e476619affccf
Beginneraboutlife116 <pythonforeye@gmail.com>
2023-05-09 17:56:03 +0800

    modify title element to solve the accessibility issue from report

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import Head from "next/head"
 import Link from "next/link"
-// import LoginPage from "../components/pages/login"
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>FEM-intro component with sign-up form</title>
+        <title>{`FEM-intro component with sign-up form`}</title>
         <meta name="description" content="intro component with sign-up form" />
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
